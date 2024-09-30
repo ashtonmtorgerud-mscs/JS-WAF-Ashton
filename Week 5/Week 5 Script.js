9b25849e017f55a479b455ebb932170f62aa52ad
Ashton Torgerud <ashtorged@outlook.com>
2024-09-30 11:50:30 -0500

    Completed Week 5

---
+++ Week 5/Week 5 Script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+window.addEventListener("load", focusName)
+// let gForm = document.forms["GoslingForm"];
+let oiMateItsABitBlurryOutTadayInnit = false;
+let literallyRyanGosling = true;
+let test = [1, 2, 3];
+
+
+function focusName(){
+    let name = gForm.elements[0].value;
+    gForm.elements[0].focus();
+    gForm.querySelector("#goslingsSelect").selectedIndex = "1";
+}
+
+
+// gForm.addEventListener("click", blurGForm)
+
+// function blurGForm(){        I have an unblur method but I'm commenting it because it's useless as Browsers automatically deselect fields when you click away,
+//     for (let i = 0; i < gForm.elements.length; i++){
+//         gForm.elements[i].blur();
+//         console.log(i);
+//     }
+// }
+
+let gForm = document.forms["GoslingForm"];
+function GetTheModal() {
+    gForm = document.forms["GoslingForm"];
+
+    Array.from(gForm.elements).forEach ( gElement => {
+        gElement.setCustomValidity("");
+    })
+
+
+    if (!gForm.checkValidity()) {
+        for (let i = 0; i < gForm.elements.length; i++) {
+            const gElement = gForm.elements[i];
+            console.log("gField value:", gForm.elements[i].value);
+
+            if (!gElement.validity.valid) {
+                if (gElement.validity.valueMissing) {
+                    gElement.setCustomValidity("You must fill this field to see if you're literally Ryan Goslin");
+                }
+                if (gElement.validity.patternMismatch) {
+                    gElement.setCustomValidity("Please reformat your input to prove you're literally Ryan Goslin");
+                }
+                gElement.reportValidity();
+                return;
+            } else{
+                gElement.setCustomValidity("");
+            }
+        };
+    }
+
+    let gModal = new bootstrap.Modal(document.getElementById('ConfirmModal'));
+    gModal.show();
+}
+
+Array.from(gForm.elements).forEach((gElement) => {
+    if (gElement.type == "Textbox"){
+        addEventListener
+    }
+});
+
+
+
+
+function clearForm(){
+    document.getElementById("GoslingForm").reset();
+}
+
+// const textFields = document.querySelectorAll('input[type="text"]');
+
+// textFields.forEach(textField => {
+//     textField.addEventListener('input', function(){
+//         this.value = textField.value;
+//     })
+// })
+
+// document.getElementById('')
